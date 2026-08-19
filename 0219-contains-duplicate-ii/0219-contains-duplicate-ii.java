a68b5cdf3fb452e127aa2a2eebbfee783eb9307e
Elvish Kansagara <129504985+elvishpatel@users.noreply.github.com>
2026-08-19 11:11:57 +0530

    Time: 37 ms (6.52%), Space: 124.8 MB (6.64%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,40 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i=0;i<nums.length;i++)
+//         {
+//            if(map.containsKey(nums[i])){
+//                 int temp = map.get(nums[i]);
+//                 if(!(Math.abs(temp - i) <= k)){
+//                     return false;
+//                 }
+//            }else{
+//             map.put(nums[i],i);
+//            }
+//         }
+//         return true;
+//     }
+// }/
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+           if(map.containsKey(nums[i])){
+                int temp = map.get(nums[i]);
+                if((Math.abs(temp - i) <= k)){
+                    return true;
+                }
+                map.put(nums[i],i);
+           }else{
+            map.put(nums[i],i);
+           }
+        }
+        return false;
+    }
+}
